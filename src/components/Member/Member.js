1f61c1f7416955f5d86073f183e00c3d24a4a04c
mdnajmul <mdnajmulovi@gmail.com>
2021-09-27 17:27:11 +0600

    display members section complete

---
+++ src/components/Member/Member.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Member.css";
+
+const Member = (props) => {
+  //   console.log(props);
+  const { name, country, designation, age, img, salary } = props.member;
+  return (
+    <div className="member">
+      <div>
+        <img style={{ width: "100px", height: "100px" }} src={img} alt=""></img>
+        <h3>
+          <span>Name:</span> {name}
+        </h3>
+        <h3>
+          <span>Designation:</span> {designation}
+        </h3>
+        <h3>
+          <span>Country:</span> {country}
+        </h3>
+        <h3>
+          <span>Age:</span> {age}
+        </h3>
+        <h3>
+          <span>Salary:</span> ${salary}
+        </h3>
+        <button className="hire-me-btn">
+          <FontAwesomeIcon icon={faShoppingCart} /> Hire Me
+        </button>
+        <p>
+          <small className="fb-icon">
+            <i class="fab fa-facebook-square icon"></i>
+          </small>
+          <small className="lind-icon">
+            <i class="fab fa-linkedin icon"></i>
+          </small>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Member;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Member from "../Member/Member";
+import "./Shop.css";
 
 const Shop = () => {
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    fetch("./software-team.JSON")
+      .then((res) => res.json())
+      .then((data) => setMembers(data));
+  }, []);
   return (
-    <div>
-      <h1>This is Shop</h1>
+    <div className="shop-container">
+      <div className="member-container">
+        {members.map((member) => (
+          <Member key={member.key} member={member}></Member>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h3>Cart Container</h3>
+      </div>
     </div>
   );
 };
