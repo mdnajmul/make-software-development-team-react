2db4cd1facc40fe07185943019e79a526be9e292
mdnajmul <mdnajmulovi@gmail.com>
2021-09-27 09:12:27 +0600

    header section complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import demo1 from './demo.jpeg'
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <img style={{width:'100px', height:'100px', borderRadius: '50%'}} src={demo1} alt=""/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Make Best Software Development Team</h1>
+      <p>
+        It is not enough to write the lines of code efficiently. You need to
+        make sure that you entrust your project to specialists who really
+        understand you.
+      </p>
+      <h1>Total Budget: 150 Million</h1>
+    </div>
+  );
+};
+
+export default Header;
