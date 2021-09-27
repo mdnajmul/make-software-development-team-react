a0b4df9523ea11569ad1bf4b5024d180dc1f4ea4
mdnajmul <mdnajmulovi@gmail.com>
2021-09-27 11:07:52 +0600

    favicon & title change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
     <div className="main">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
